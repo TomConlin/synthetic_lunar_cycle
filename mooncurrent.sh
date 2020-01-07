42ca400ebb9b05cc3a6406ebe8f62792292db05c
TomConlin <corporate.drone1339@gmail.com>
2020-01-06 23:54:24 -0800

    switch to choosing image via pom2percent.awk

--- mooncurrent.sh
+++ mooncurrent.sh
@@ -4,6 +4,9 @@ HOME=/home/users/tomc
 SRCDIR=$HOME/GitHub/synthetic_lunar_cycle/out
 POMDIR=$HOME/bin
 DSTDIR=$HOME/public_html/images 
-CUR=$(printf "%3.3i.png" $($POMDIR/pom|/bin/sed -n 's/.*(\([0-9]\+\)% .*)/\1/p')) 
+# insufficent, pom uses % of full either side of full & other words
+# CUR=$(printf "%3.3i.png" $($POMDIR/pom|/bin/sed -n 's/.*(\([0-9]\+\)% .*)/\1/p'))
+
+CUR = $($POMDIR/pom | ./pom2percent.awk)
 
 /bin/ln --force --no-dereference --symbolic $SRCDIR/$CUR $DSTDIR/moon.png
